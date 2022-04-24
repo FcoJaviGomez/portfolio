5d1102a4f429dc5614acb2bc9c81cb9f760fa7f2
FcoJavierGómez <javiergomez742@gmail.com>
2022-04-24 17:19:17 +0200

    make it better

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const expres = require('express');
 const app = expres();
 
-app.use(expres.static(__dirname + '/dist/auto-mate'));
+app.use(expres.static(__dirname + '/dist/portfolio'));
 app.get('/*', function (req, res) {
-    res.sendFile("index.html", { root: __dirname + '/dist/auto-mate/' });
+    res.sendFile("index.html", { root: __dirname + '/dist/portfolio/' });
 });
 
 app.listen(process.env.PORT || 3000);
