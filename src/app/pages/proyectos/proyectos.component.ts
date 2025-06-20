22c1146724601b63227c21abf64a1f99e0394979
Fco Javier Gómez Galán <javiergomez742@gmail.com>
2025-06-20 11:33:44 +0200

    Modificacion proyectos y cv

--- src/app/pages/proyectos/proyectos.component.ts
+++ src/app/pages/proyectos/proyectos.component.ts
@@ -12,15 +12,12 @@ export class ProyectosComponent implements OnInit {
 
 
   constructor() {
-    this.proyectos = [new Proyecto("AutoMate", "App para la gestión de mantenimientos del coche",
-      "https://auto-mate.netlify.app/", "https://github.com/FcoJaviGomez/autoMate",
-      "../../../assets/automateproyecto.PNG"),
-    new Proyecto("Api Pokemon", "Pagina web, llamando a una api pública",
+    this.proyectos = [new Proyecto("CineClub", "Aplicación web, fanáticos del cine",
+      "https://cineclubb.netlify.app/login", "https://github.com/FcoJaviGomez/CineClub1",
+      "../../../assets/cineclub.png"),
+    new Proyecto("Api Pokemon", "Página web, llamando a una api pública",
       "https://pokemon2d.netlify.app/", "https://github.com/FcoJaviGomez/Pokemon",
-      "../../../assets/lugia.jpg"),
-    new Proyecto("House Book", "Control y gestión de libros en propiedad",
-      "https://house-book.herokuapp.com/", "https://github.com/FcoJaviGomez/MyBooks",
-      "../../../assets/books.png")]
+      "../../../assets/lugia.jpg")]
   }
 
   ngOnInit(): void {
